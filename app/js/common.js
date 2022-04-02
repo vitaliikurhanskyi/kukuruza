291ab26a9e7920bdea865d4c0e96687624aff2fc
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2022-04-02 18:22:35 +0300

    02.04.2022

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,23 @@
-console.log('test');
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var menu = document.getElementById('modal-menu-position');
+    var menuBtn = document.getElementById('menu-btn');
+    var closeMenu = document.getElementById('close-menu');
+
+    menuBtn.onclick = function(e){
+
+        e.preventDefault();
+
+        menu.classList.add('toogle-menu');
+
+    }
+
+    closeMenu.onclick = function(e){
+
+        e.preventDefault();
+
+        menu.classList.remove('toogle-menu');
+
+    }
+
+});
